b63cba10c957885fa954ad7ceda7473da04e2f86
IsmaelSailema20 <ismaelsailema110@gmail.com>
2024-06-19 14:14:53 -0500

    Controles en carrito y cruds para editar

--- src/carrito_compras/carrito_compras.controller.ts
+++ src/carrito_compras/carrito_compras.controller.ts
@@ -3,6 +3,8 @@ import { CarritoComprasService } from './carrito_compras.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateCarritoComprasDto } from './dtos/create-carrito_compras.dto';
 import { UpdateCarritoCompras } from './dtos/update-carrito_compras.dto';
+import { UpdateCarritoEstadoDto } from './dtos/update-estado-carrito_compras.dto';
+import { UpdateProductoCarritoDto } from './dtos/update-Cantidad-Productos-carrito.dto';
 @ApiTags('Carrito-De-Compras')
 @Controller('carrito-compras')
 export class CarritoComprasController {
@@ -28,6 +30,28 @@ export class CarritoComprasController {
   ) {
     return this.carritoComprasService.addProductosToCarrito(
       addProductosCarritoDto,
+    );
+  }
+
+  @Patch(':id/estado')
+  async updateCarritoEstado(
+    @Param('id') id_usuario: number,
+    @Body() updateCarritoEstadoDto: UpdateCarritoEstadoDto,
+  ) {
+    return this.carritoComprasService.updateCarritoEstado(
+      id_usuario,
+      updateCarritoEstadoDto,
+    );
+  }
+
+  @Patch(':id_carrito/producto')
+  async updateProductoCantidad(
+    @Param('id_carrito') id_carrito: number,
+    @Body() updateProductoCarritoDto: UpdateProductoCarritoDto,
+  ) {
+    return this.carritoComprasService.updateProductoCantidad(
+      id_carrito,
+      updateProductoCarritoDto,
     );
   }
 }
